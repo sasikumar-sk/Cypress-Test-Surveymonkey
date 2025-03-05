8db57b56c84533ad0d37f6ba30131a4adc2e2f80
sasi <DCKLP-125@DC-KA-QA-L-256.desicrew.in>
2025-03-05 09:18:32 +0530

    Updated code commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,27 @@
 const { defineConfig } = require("cypress");
+const mochawesomeMerge = require('mochawesome-merge');
+const reportGenerator = require('mochawesome-report-generator');
 
+const fs = require('fs');
+const path = require('path');
+
+ 
 module.exports = defineConfig({
   e2e: {
-
     pageLoadTimeout: 120000, 
     experimentalStudio: true,
     chromeWebSecurity: false,
     failOnStatusCode: true,
     video: false,
     screenshotOnRunFailure: false,
-      specPattern:'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    setupNodeEvents(on, config) {
-     
-     
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: true,
+      html: false,
+      json: true  
     },
   },
 });
